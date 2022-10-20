9db4ddf838be3f77b3130ac9842848da8d7d7d80
Rykala <szymon.rykala@accenture.com>
2022-10-20 11:41:08 +0200

    make not as a package

--- lotto/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-    package for lotto cli project
-'''
-
-__app_name__ = "lotto"
-__version__ = "0.0.1"
--- lotto/__main__.py
+++ lotto/__main__.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+
+__app_name__ = "lotto"
+__version__ = "0.0.2"
+
+
 from cli import app
 
 
